12d81f1041a5e6d337b3c590dd1cb7686a31db55
Muhammad Ibrahim <113703307+EmIbrahim@users.noreply.github.com>
2025-12-01 00:58:48 +0500

    Update firebase.config.ts

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -9,12 +9,12 @@ import { getAuth } from 'firebase/auth'
 // Your Firebase configuration object
 // Replace these with your actual values from Firebase Console
 const firebaseConfig = {
-  apiKey: "AIzaSyALnx_amaM7jLLzDvTRZZZFWFVD4ZlTmFM",
+  apiKey: "APIKEY",
   authDomain: "hci-air-project.firebaseapp.com",
   projectId: "hci-air-project",
   storageBucket: "hci-air-project.firebasestorage.app",
-  messagingSenderId: "972050466470",
-  appId: "1:972050466470:web:505b38f9c74057efa55667"
+  messagingSenderId: "SENDERID",
+  appId: "APPID"
 }
 
 // Initialize Firebase
